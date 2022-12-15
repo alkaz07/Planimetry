d94ed3d2b47bbcfbbf5cbe1b6c3fd94b4dac50a6
Aleksandra8888888 <froska@list.ru>
2022-12-15 14:08:48 +0300

    Triangle

---
+++ src/Triangle.java
@@ -0,0 +1,30 @@
+public class Triangle {
+   Point Ver1;
+   Point Ver2;
+   Point Ver3;
+
+    public Triangle(Point Ver1,Point Ver2,Point Ver3){
+        this.Ver1=Ver1;
+        this.Ver2=Ver2;
+        this.Ver3=Ver3;
+    }
+      public Triangle(double x1,double y1,double x2,double y2,double x3,double y3){
+        Ver1=new Point(x1,y1);
+        Ver2=new Point(x2,y2);
+        Ver3=new Point(x3,y3);
+      }
+
+    public double getLength1(){
+        return Point.getDistance(Ver1,Ver2);
+    }
+    public double getLength2(){
+        return Point.getDistance(Ver2,Ver3);
+    }
+    public double getLength3() {
+        return Point.getDistance(Ver3, Ver1);
+    }
+
+    public double getSumLength(){
+        return (getLength1()+getLength2()+getLength3());
+    }
+}
